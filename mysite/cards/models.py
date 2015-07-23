3b995b2129a81c675a5931a130e58f53fbdd9d9a
cornelM <manolescu_cornel@yahoo.ro>
2015-07-23 13:28:36 +0300

    writing views

--- mysite/cards/models.py
+++ mysite/cards/models.py
@@ -1,15 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Card(models.Model):
+
+    owner = models.ForeignKey(User)
     url = models.URLField()
     name = models.CharField(max_length=60)
     image = models.ImageField()
     media_type = models.CharField(max_length=35)
     created_at = models.DateTimeField('date created')
 
-    def __repr__(self):
-        return '<{} - {}>'.format(self.id, self.name)
+#    def __repr__(self):
+ #       return '<{} - {}>'.format(self.id, self.name)
     def __str__(self):
         return self.name
